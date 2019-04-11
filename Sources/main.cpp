7050066a4ec92956bc45b66b0d32076bfc3c3f5e
Tony Di Malta <Sky-Angels8@hotmail.fr>
2019-04-11 21:23:17 +0200

    Improves successive function calls
    Adds a pause after each call that does not depend on the OS or compiler.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,20 +1,47 @@
 #ifdef _DEBUG
-    #include <iostream>
-    extern void print_type_info_tests();
-    extern void print_unique_random_sizes();
-    extern void print_any_type_tests();
+#include <iostream>
+#include <limits>
+
+extern void print_type_info_tests();
+extern void print_unique_random_sizes();
+extern void print_any_type_tests();
+
+void flush(std::istream& in)
+{
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    in.clear();
+}
+
+void pause()
+{
+    std::cout << "Press [Enter] to continue...";
+    flush(std::cin);
+}
+
+template <class Fct>
+void call_functions_with_pause(Fct && fct)
+{
+    fct();
+    pause();
+    std::cout << '\n';
+}
+
+template <class Fct, class... Fcts>
+void call_functions_with_pause(Fct && fct, Fcts && ... fcts)
+{
+    call_functions_with_pause(fct);
+    call_functions_with_pause(std::forward<Fcts>(fcts)...);
+}
 #endif
 
 int main()
 {
 #ifdef _DEBUG
-    print_type_info_tests();
-    system("pause");
-    std::cout << '\n';
-    print_unique_random_sizes();
-    system("pause");
-    std::cout << '\n';
-    print_any_type_tests();
+    call_functions_with_pause(
+        print_type_info_tests,
+        print_unique_random_sizes,
+        print_any_type_tests
+    );
 #endif
 
     return 0;
